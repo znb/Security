37af17ae56ec56b755e9de1617f6fb1b885738d8
Matt <github@zonbi.org>
2018-02-20 17:38:18 +0000

    Looks like Python2 is still king

--- TheHive/files/post_install-cortex.sh
+++ TheHive/files/post_install-cortex.sh
@@ -11,8 +11,8 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -o Dpkg::Options::="--force-conf
 sudo echo 'deb https://dl.bintray.com/cert-bdf/debian any main' | sudo tee -a /etc/apt/sources.list.d/thehive-project.list
 sudo apt-key adv --keyserver hkp://pgp.mit.edu --recv-key 562CBC1C
 sudo apt -qq update
-sudo apt -yqq --allow-unauthenticated install cortex git language-pack-en python3-pip
-sudo pip3 install --upgrade pip
+sudo apt -yqq --allow-unauthenticated install cortex git language-pack-en python-pip
+sudo pip2 install --upgrade pip
 
 # Cortex Analyzers
 sudo git clone https://github.com/TheHive-Project/Cortex-Analyzers.git /opt/Cortex-Analyzers
@@ -20,7 +20,7 @@ sudo git clone https://github.com/Yara-Rules/rules.git /opt/Yara-Rules
 sudo git clone https://github.com/firehol/blocklist-ipsets /opt/FireHOLBlocklists/blocklists
 sudo git clone https://github.com/MISP/misp-warninglists /opt/misp-warninglists
 
+
 # Misc
-sudo ln -s /usr/bin/python3 /usr/bin/python
 # Ensure we are enabled
 sudo systemctl enable cortex.service
